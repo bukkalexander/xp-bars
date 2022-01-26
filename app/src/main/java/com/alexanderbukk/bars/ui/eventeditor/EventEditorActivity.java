a63cd1b566fb3bea3e6aeebe0d39efa09e7cdd01
alexa <alexander.bukk@gmail.com>
2022-01-26 17:57:14 +0100

    Add date picker

--- app/src/main/java/com/alexanderbukk/bars/ui/eventeditor/EventEditorActivity.java
+++ app/src/main/java/com/alexanderbukk/bars/ui/eventeditor/EventEditorActivity.java
@@ -2,10 +2,20 @@ package com.alexanderbukk.bars.ui.eventeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.alexanderbukk.bars.R;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 public class EventEditorActivity extends AppCompatActivity {
 
@@ -26,6 +36,27 @@ public class EventEditorActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Save to database
                 onBackPressed();
+            }
+        });
+
+        TextView tvStartDate = findViewById(R.id.tvStartDate);
+
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                LocalDate now = LocalDate.of(year, month+1, day);
+                String dateStr = now.format(DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy"));
+                tvStartDate.setText(dateStr);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+        
+
+        tvStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
             }
         });
     }
