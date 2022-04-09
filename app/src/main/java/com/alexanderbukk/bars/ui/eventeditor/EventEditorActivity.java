858a65d885e845c968b6e958aca99eb1e56c27cd
abukk <abukk@volvocars.com>
2022-04-09 21:42:49 +0200

    refactor (EventEditorActivity): convert variables to fields

--- app/src/main/java/com/alexanderbukk/bars/ui/eventeditor/EventEditorActivity.java
+++ app/src/main/java/com/alexanderbukk/bars/ui/eventeditor/EventEditorActivity.java
@@ -10,12 +10,16 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.alexanderbukk.bars.R;
+import com.alexanderbukk.bars.data.eventinstance.EventInstance;
 import com.alexanderbukk.bars.data.group.Group;
 import com.alexanderbukk.bars.ui.event.EventViewModel;
 
@@ -31,6 +35,34 @@ public class EventEditorActivity extends AppCompatActivity {
 
     private EventEditorViewModel eventEditorViewModel;
 
+    private ImageButton ibClose;
+    private Button btnSave;
+    private EditText etTitle;
+    private ImageView ivCategory;
+    private TextView tvCategory;
+    private TextView tvEvent;
+    private TextView tvStartDate;
+    private TextView tvEndDate;
+    private TextView tvStartTime;
+    private TextView tvEndTime;
+    private EditText etDescription;
+    private EditText etBarsExtra;
+    private TextView tvBarsPerOccurrenceNum;
+    private TextView tvBarsPerHourNum;
+    private TextView tvBarsForYesterdayNum;
+    private TextView tvBarsPerOccurrenceLimitNum;
+    private TextView tvBarsDailyLimitNum;
+    private TextView tvBarsTotalCalc;
+    private TextView tvBarsTotalNum;
+
+
+    private Calendar calendarStart;
+    private Calendar calendarEnd;
+    private DatePickerDialog datePickerDialogStart;
+    private DatePickerDialog datePickerDialogEnd;
+    private TimePickerDialog timePickerDialogStart;
+    private TimePickerDialog timePickerDialogEnd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,43 +75,50 @@ public class EventEditorActivity extends AppCompatActivity {
         eventEditorViewModel = new ViewModelProvider(this).get(EventEditorViewModel.class);
 
         // =========================================================================================
-        // Buttons
+        // Get views by id
         // =========================================================================================
-        findViewById(R.id.ibClose).setOnClickListener(new View.OnClickListener() {
+        ibClose = findViewById(R.id.ibClose);
+        btnSave = findViewById(R.id.btnSave);
+        etTitle = findViewById(R.id.etTitle);
+        ivCategory = findViewById(R.id.ivCategory);
+        tvCategory = findViewById(R.id.tvCategory);
+        tvEvent = findViewById(R.id.tvEvent);
+        tvStartDate = findViewById(R.id.tvStartDate);
+        tvEndDate = findViewById(R.id.tvEndDate);
+        tvStartTime = findViewById(R.id.tvStartTime);
+        tvEndTime = findViewById(R.id.tvEndTime);
+        etDescription = findViewById(R.id.etDescription);
+        etBarsExtra = findViewById(R.id.etBarsExtra);
+        tvBarsPerOccurrenceNum = findViewById(R.id.tvBarsPerOccurrenceNum);
+        tvBarsPerHourNum = findViewById(R.id.tvBarsPerHourNum);
+        tvBarsForYesterdayNum = findViewById(R.id.tvBarsForYesterdayNum);
+        tvBarsPerOccurrenceLimitNum = findViewById(R.id.tvBarsPerOccurrenceLimitNum);
+        tvBarsDailyLimitNum = findViewById(R.id.tvBarsDailyLimitNum);
+        tvBarsTotalCalc = findViewById(R.id.tvBarsTotalCalc);
+        tvBarsTotalNum = findViewById(R.id.tvBarsTotalNum);
+
+        // =========================================================================================
+        // Set button on click listeners
+        // =========================================================================================
+        ibClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
-
-        findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Save to database
+                createEventInstance();
+
+                // Switch back to previous activity
                 onBackPressed();
             }
         });
-
-
-        // =========================================================================================
-        // Title
-        // =========================================================================================
-        findViewById(R.id.etTitle);
-
-
-        // =========================================================================================
-        // Category (group,event)
-        // =========================================================================================
-        ImageView ivCategory = findViewById(R.id.ivCategory);
 //        ivCategory.setColorFilter(eventEditorViewModel.getGroupByName(groupName).color);
-
-        TextView tvCategory = findViewById(R.id.tvCategory);
         tvCategory.setText(groupName);
-
-
-        TextView tvEvent = findViewById(R.id.tvEvent);
         tvEvent.setText(eventName);
-
         eventEditorViewModel.getAllGroups().observe(this, new Observer<List<Group>>() {
             @Override
             public void onChanged(@Nullable List<Group> group) {
@@ -89,13 +128,11 @@ public class EventEditorActivity extends AppCompatActivity {
 
 
         // =========================================================================================
-        // Date
+        // Initialize calendar and date pickers
         // =========================================================================================
-        // Start
-        TextView tvStartDate = findViewById(R.id.tvStartDate);
-
-        Calendar calendarStart = Calendar.getInstance();
-        DatePickerDialog datePickerDialogStart = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+        // Start date
+        calendarStart = Calendar.getInstance();
+        datePickerDialogStart = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 LocalDate now = LocalDate.of(year, month+1, day);
@@ -111,11 +148,9 @@ public class EventEditorActivity extends AppCompatActivity {
             }
         });
 
-        // End
-        TextView tvEndDate = findViewById(R.id.tvEndDate);
-
-        Calendar calendarEnd = Calendar.getInstance();
-        DatePickerDialog datePickerDialogEnd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+        // End date
+        calendarEnd = Calendar.getInstance();
+        datePickerDialogEnd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 LocalDate now = LocalDate.of(year, month+1, day);
@@ -135,9 +170,8 @@ public class EventEditorActivity extends AppCompatActivity {
         // =========================================================================================
         // Time
         // =========================================================================================
-        // Start
-        TextView tvStartTime = findViewById(R.id.tvStartTime);
-        TimePickerDialog timePickerDialogStart = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+        // Start time
+        timePickerDialogStart = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
                 LocalTime now = LocalTime.of(hourOfDay, minute);
@@ -153,9 +187,8 @@ public class EventEditorActivity extends AppCompatActivity {
             }
         });
 
-        // End
-        TextView tvEndTime = findViewById(R.id.tvEndTime);
-        TimePickerDialog timePickerDialogEnd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+        // End time
+        timePickerDialogEnd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
                 LocalTime now = LocalTime.of(hourOfDay, minute);
@@ -171,22 +204,14 @@ public class EventEditorActivity extends AppCompatActivity {
             }
         });
 
-        // =========================================================================================
-        // Description
-        // =========================================================================================
-        findViewById(R.id.etDescription);
+    }
 
-        // =========================================================================================
-        // Bars
-        // =========================================================================================
-        findViewById(R.id.etBarsExtra);
-        findViewById(R.id.tvBarsPerOccurrenceNum);
-        findViewById(R.id.tvBarsPerHourNum);
-        findViewById(R.id.tvBarsForYesterdayNum);
-        findViewById(R.id.tvBarsPerOccurrenceLimitNum);
-        findViewById(R.id.tvBarsDailyLimitNum);
-        findViewById(R.id.tvBarsTotalCalc);
-        findViewById(R.id.tvBarsTotalNum);
+    private void createEventInstance() {
 
+//        EventInstance eventInstance = new EventInstance(
+//                findViewById(R.id.etTitle).,
+//
+//        );
+        Log.d(this.getLocalClassName(), "createEventInstance() called");
     }
 }
