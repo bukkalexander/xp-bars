850923892d96d7e01d1eaecdfbe386c8e3128230
abukk <abukk@volvocars.com>
2022-04-09 21:40:29 +0200

    feature (EventEditorViewModel): add EventInstance
    Makes it possible to get and insert into the EventInstance db table

--- app/src/main/java/com/alexanderbukk/bars/ui/eventeditor/EventEditorViewModel.java
+++ app/src/main/java/com/alexanderbukk/bars/ui/eventeditor/EventEditorViewModel.java
@@ -7,6 +7,8 @@ import androidx.lifecycle.LiveData;
 
 import com.alexanderbukk.bars.data.event.Event;
 import com.alexanderbukk.bars.data.event.EventRepository;
+import com.alexanderbukk.bars.data.eventinstance.EventInstance;
+import com.alexanderbukk.bars.data.eventinstance.EventInstanceRepository;
 import com.alexanderbukk.bars.data.group.Group;
 import com.alexanderbukk.bars.data.group.GroupRepository;
 
@@ -18,20 +20,29 @@ public class EventEditorViewModel extends AndroidViewModel {
 
     private GroupRepository groupRepository;
     private EventRepository eventRepository;
+    private EventInstanceRepository eventInstanceRepository;
+
     private final LiveData<List<Event>> allEvents;
     private final LiveData<List<Group>> allGroups;
+    private final LiveData<List<EventInstance>> allEventInstances;
 
 
     public EventEditorViewModel(Application application) {
         super(application);
         eventRepository = new EventRepository(application);
         groupRepository = new GroupRepository(application);
+        eventInstanceRepository = new EventInstanceRepository(application);
+
         allEvents = eventRepository.getAllEvents();
         allGroups = groupRepository.getAllGroups();
+        allEventInstances = eventInstanceRepository.getAllEvents();
     }
 
     public LiveData<List<Event>> getAllEvents() {
         return allEvents;
+    }
+    public LiveData<List<EventInstance>> getAllEventInstances() {
+        return allEventInstances;
     }
     public LiveData<List<Group>> getAllGroups() {
         return allGroups;
